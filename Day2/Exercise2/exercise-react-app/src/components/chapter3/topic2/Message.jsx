fc3687ed51e18ccd6b719612a530c2be0701f0c7
Priyanka Mathur <priyanka.mathur@henkel.com>
2025-09-04 01:00:30 +0530

    deleted exercse 2 folder

--- Day2/Exercise2/exercise-react-app/src/components/chapter3/topic2/Message.jsx
+++
@@ -1,9 +0,0 @@
-export default function Message({ text = "", name = "", about = "" }) {
-  return (
-    <>
-      <p>{text}</p>
-      <p><strong>{name}</strong></p>
-      <p>{about}</p>
-    </>
-  );
-}
